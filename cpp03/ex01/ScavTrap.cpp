629fff199a1ac24ef5711d28f3a9b01a866f897a
Emma Benoist <emma.benoist91@gmail.com>
2025-12-03 17:40:42 +0100

    Add files via upload

--- cpp03/ex01/ScavTrap.cpp
+++ cpp03/ex01/ScavTrap.cpp
@@ -1,12 +1,37 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ScavTrap.cp                                        :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: ebenoist <ebenoist@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2025/12/03 15:51:08 by ebenoist          #+#    #+#              #
-#    Updated: 2025/12/03 15:51:09 by ebenoist         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebenoist <ebenoist@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/03 17:05:02 by ebenoist          #+#    #+#             */
+/*   Updated: 2025/12/03 17:18:24 by ebenoist         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap(const std::string name) : ClapTrap(name)
+{
+	this-> Hit_points = 100;
+	this-> Energy_points = 50;
+	this-> Attack_damage = 20;
+	std::cout << "ScavTrap default constructor called for " << _name << std::endl;
+	
+}
+
+ScavTrap::~ScavTrap()
+{
+	
+}
+
+ScavTrap::ScavTrap (const ScavTrap& other)
+{
+
+}
+
+ScavTrap& ScavTrap::operator=(const ScavTrap& other)
+{
+	
+}
--- cpp03/ex01/ScavTrap.hpp
+++ cpp03/ex01/ScavTrap.hpp
@@ -6,7 +6,26 @@
 /*   By: ebenoist <ebenoist@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 15:51:34 by ebenoist          #+#    #+#             */
-/*   Updated: 2025/12/03 15:51:35 by ebenoist         ###   ########.fr       */
+/*   Updated: 2025/12/03 17:11:46 by ebenoist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
+
+#include <iostream>
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap{
+
+public: 
+	ScavTrap(std::string name);
+	~ScavTrap();
+	ScavTrap (const ScavTrap& other);
+	ScavTrap& operator=(const ScavTrap& other);
+
+private:
+
+};
+
+#endif
